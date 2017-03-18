cddc7e58f6097c45e5865b918f6bb1488e254b18
Roshan Bhandari <brishi98@gmail.com>
2017-03-18 19:14:57 +0545

    changes

--- fbstats/users/urls.py
+++ fbstats/users/urls.py
@@ -13,10 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
-from django.contrib import admin
-from django.views.generic import TemplateView
-from django.views.decorators.cache import cache_page
+from __future__ import absolute_import, unicode_literals
+
+from django.conf.urls import url
+
+from . import views
 
 
 urlpatterns = [
