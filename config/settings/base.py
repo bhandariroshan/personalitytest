9b9af3e8134c7fc3f3d920afe882db55de98f82c
Roshan Bhandari <brishi98@gmail.com>
2017-03-25 13:07:43 +0545

    celery changes

--- config/settings/base.py
+++ config/settings/base.py
@@ -298,8 +298,68 @@ BROKER_URL = "amqp://fbstats:fbstats@localhost:5672/myvhost"
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = 'Asia/Kathmandu'
+
 CELERY_RESULT_BACKEND = 'djcelery.backends.database:DatabaseBackend'
 CELERYBEAT_SCHEDULER = 'djcelery.schedulers.DatabaseScheduler'
 
 os.environ["CELERY_LOADER"] = "django"
+
+
+REDIS_PORT = 6379  
+REDIS_DB = 0  
+REDIS_HOST = os.environ.get('REDIS_PORT_6379_TCP_ADDR', 'redis')
+
+RABBIT_HOSTNAME = os.environ.get('RABBIT_PORT_5672_TCP', 'rabbit')
+
+if RABBIT_HOSTNAME.startswith('tcp://'):  
+    RABBIT_HOSTNAME = RABBIT_HOSTNAME.split('//')[1]
+
+# BROKER_URL = os.environ.get('BROKER_URL',  
+#                             '')
+# if not BROKER_URL:  
+#     BROKER_URL = 'amqp://{user}:{password}@{hostname}/{vhost}/'.format(
+#         user=os.environ.get('RABBIT_ENV_USER', 'admin'),
+#         password=os.environ.get('RABBIT_ENV_RABBITMQ_PASS', 'mypass'),
+#         hostname=RABBIT_HOSTNAME,
+#         vhost=os.environ.get('RABBIT_ENV_VHOST', ''))
+
+# We don't want to have dead connections stored on rabbitmq, so we have to negotiate using heartbeats
+# BROKER_HEARTBEAT = '?heartbeat=30'  
+# if not BROKER_URL.endswith(BROKER_HEARTBEAT):  
+#     BROKER_URL += BROKER_HEARTBEAT
+
+BROKER_POOL_LIMIT = 1  
+BROKER_CONNECTION_TIMEOUT = 10
+
+# Celery configuration
+
+# configure queues, currently we have only one
+CELERY_DEFAULT_QUEUE = 'default'  
+CELERY_QUEUES = (  
+    Queue('default', Exchange('default'), routing_key='default'),
+)
+
+# Sensible settings for celery
+CELERY_ALWAYS_EAGER = False  
+CELERY_ACKS_LATE = True  
+CELERY_TASK_PUBLISH_RETRY = True  
+CELERY_DISABLE_RATE_LIMITS = False
+
+# By default we will ignore result
+# If you want to see results and try out tasks interactively, change it to False
+# Or change this setting on tasks level
+CELERY_IGNORE_RESULT = True  
+CELERY_SEND_TASK_ERROR_EMAILS = False  
+CELERY_TASK_RESULT_EXPIRES = 600
+
+# Set redis as celery result backend
+
+CELERY_REDIS_MAX_CONNECTIONS = 1
+
+# Don't use pickle as serializer, json is much safer
+CELERY_TASK_SERIALIZER = "json"  
+CELERY_ACCEPT_CONTENT = ['application/json']
+
+CELERYD_HIJACK_ROOT_LOGGER = False  
+CELERYD_PREFETCH_MULTIPLIER = 1  
+CELERYD_MAX_TASKS_PER_CHILD = 1000  
