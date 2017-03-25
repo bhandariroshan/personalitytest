cefdf3f85bb3f9e15c3f78a63f4adc76ea76f683
Roshan Bhandari <brishi98@gmail.com>
2017-03-25 13:19:42 +0545

    changes

--- compose/django/run_celery.sh
+++ compose/django/run_celery.sh
@@ -5,4 +5,4 @@ sleep 10
 
 cd myproject  
 # run Celery worker for our project myproject with Celery configuration stored in Celeryconf
-su -m myuser -c "celery worker -A myproject.celeryconf -Q default -n default@%h"  
+su -m myuser -c "celery worker -A mainapp.celeryconf -Q default -n default@%h"
