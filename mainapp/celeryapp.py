36769776b6030691d03561d51be41ddcc75299d3
Roshan Bhandari <brishi98@gmail.com>
2017-03-25 23:06:53 +0545

    changes

--- mainapp/celeryapp.py
+++ mainapp/celeryapp.py
@@ -6,7 +6,7 @@ from django.conf import settings
 # Indicate Celery to use the default Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fbstats.config.settings.base')
 
-app = CeleryClass('mainapp')
+app = CeleryClass('fbstats.mainapp')
 app.config_from_object('django.conf:settings')
 # This line will tell Celery to autodiscover all your tasks.py
 # that are in your app folders
