ed0672e9a6a046abdf0a8dad9c34399c5d830184
Roshan Bhandari <brishi98@gmail.com>
2017-03-27 18:02:27 +0545

    task changes

--- mainapp/tasks.py
+++ mainapp/tasks.py
@@ -3,7 +3,10 @@ from celery.decorators import periodic_task
 from celery.utils.log import get_task_logger
 
 from .pull_likes import get_myfacebook_likes
-from action.models import UserData
+from action.models import UserData, UserLikes
+import requests
+import json
+import facebook
 
 logger = get_task_logger(__name__)
 
@@ -14,16 +17,39 @@ def pull_user_likes():
     logger.info("Start task of pulling user likes")
     user_datas = UserData.objects.filter(likes_pulled=False)
     print(user_datas)
+
     for each_user in user_datas:
+        print(each_user)
+
         tokens = SocialToken.objects.filter(
             account__user=each_user.user,
             account__provider='facebook'
         )
+        myfbgraph = facebook.GraphAPI(tokens[0].token)
+        myfacebook_likes_info = myfbgraph.get_connections("me", "likes")
 
         print(tokens)
 
-        myfbgraph = facebook.GraphAPI(tokens[0].token)
-        my_likes = get_myfacebook_likes(myfbgraph, each_user.user)
+        myfacebook_likes = []
+        while myfacebook_likes_info['data']:
+            for like in myfacebook_likes_info['data']:
+                myfacebook_likes.append(like)
+
+                user_like  = UserLikes.objects.get_or_create(user=each_user.user, like=like)
+                user_like[0].like = like
+                user_like[0].save()
+
+            if 'next' in myfacebook_likes_info['paging'].keys():
+                myfacebook_likes_info = requests.get(myfacebook_likes_info['paging']['next']).json()
+            else:
+                break
+
+        print("CALLED")
+        
+        each_user.likes_pulled = True
+        each_user.save()
+
 
         print("COMPLETE")
+
     logger.info("Task finished: result = %i" % bool(result))
