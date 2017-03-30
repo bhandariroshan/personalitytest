e08676a193be03c7676c590c577c37570256c1eb
Roshan Bhandari <brishi98@gmail.com>
2017-03-30 22:18:16 +0545

    production.py changes

--- config/settings/production.py
+++ config/settings/production.py
@@ -72,6 +72,27 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS += ['gunicorn', ]
 
+INSTALLED_APPS += [
+    # 'django.contrib.admin',
+    # 'django.contrib.auth',
+    # 'django.contrib.contenttypes',
+    # 'django.contrib.sessions',
+    # 'django.contrib.messages',
+    # 'django.contrib.staticfiles',
+    # 'django.contrib.sites',
+    'rest_framework',
+    # allauth
+    # 'allauth',
+    # 'allauth.account',
+    # 'allauth.socialaccount',
+    'allauth.socialaccount.providers.facebook',
+    # 'allauth.socialaccount.providers.twitter',
+    # 'djcelery',
+    'celery',
+    # # local apps
+    'mainapp',
+    'action',
+]
 
 # STORAGE CONFIGURATION
 # ------------------------------------------------------------------------------
