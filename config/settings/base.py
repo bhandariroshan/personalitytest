89a5610c1b9f794bdcec4b384fd20689232bee63
Roshan Bhandari <brishi98@gmail.com>
2017-03-31 22:22:56 +0545

    changes

--- config/settings/base.py
+++ config/settings/base.py
@@ -263,7 +263,7 @@ SOCIALACCOUNT_ADAPTER = 'fbstats.users.adapters.SocialAccountAdapter'
 # Custom user app defaults
 # Select the correct user model
 AUTH_USER_MODEL = 'users.User'
-LOGIN_REDIRECT_URL = 'users:redirect'
+LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = 'account_login'
 
 # SLUGLIFIER
