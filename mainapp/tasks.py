863cdf59e3fd446ae94539d926498f3f70258230
Roshan Bhandari <brishi98@gmail.com>
2017-04-02 12:03:35 +0545

    changes

--- mainapp/tasks.py
+++ mainapp/tasks.py
@@ -17,8 +17,7 @@ logger = get_task_logger(__name__)
 def pull_user_likes():
     logger.info("Start task of pulling user likes")
     user_datas = UserData.objects.filter(likes_pulled=False)
-    print(user_datas)
-
+    
     for each_userdata in user_datas:
         tokens = SocialToken.objects.filter(
             account__user=each_userdata.user,
@@ -31,11 +30,14 @@ def pull_user_likes():
         myfacebook_likes = []
         while myfacebook_likes_info['data']:
             for like in myfacebook_likes_info['data']:
+                print(like)
                 myfacebook_likes.append(like)
 
                 user_like  = UserLikes.objects.get_or_create(user=each_userdata.user, like=like)
+                print(user_like)
                 user_like[0].like = like
                 user_like[0].save()
+                print("Saved")
 
             if 'next' in myfacebook_likes_info['paging'].keys():
                 myfacebook_likes_info = requests.get(myfacebook_likes_info['paging']['next']).json()
