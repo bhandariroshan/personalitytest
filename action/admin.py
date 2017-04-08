da2f9c7489390c5acc1a38eabb63c771facdcf8a
Roshan Bhandari <brishi98@gmail.com>
2017-04-08 10:11:40 +0545

    admin changes

--- action/admin.py
+++ action/admin.py
@@ -6,10 +6,10 @@ from action.models import UserData, UserLikes
 
 @admin.register(UserData)
 class TermResultAdmin(admin.ModelAdmin):
-    list_display = ('user')
-    search_fields = ('user')
+    list_display = ('user','likes_pulled',)
+    search_fields = ('user',)
 
 @admin.register(UserLikes)
 class TermResultAdmin(admin.ModelAdmin):
-    list_display = ('user')
+    list_display = ('user',)
     search_fields = ('user')
