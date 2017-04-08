33f4f24be30ea858a47581ef1e57c8cd03335fef
Roshan Bhandari <brishi98@gmail.com>
2017-04-08 10:09:07 +0545

    changes

--- action/admin.py
+++ action/admin.py
@@ -3,5 +3,13 @@ from django.contrib import admin
 # Register your models here.
 from action.models import UserData, UserLikes
 
-admin.site.register(UserData)
-admin.site.register(UserLikes)
+
+@admin.register(UserData)
+class TermResultAdmin(admin.ModelAdmin):
+    list_display = ('user')
+    search_fields = ('user')
+
+@admin.register(UserLikes)
+class TermResultAdmin(admin.ModelAdmin):
+    list_display = ('user')
+    search_fields = ('user')
