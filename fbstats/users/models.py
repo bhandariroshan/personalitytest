d0ab66273cd10e1611b6a9ba5ab9e352a7ddfdec
bhandariroshan <brishi98@gmail.com>
2023-02-25 13:07:36 -0500

    Update models.py

--- fbstats/users/models.py
+++ fbstats/users/models.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals, absolute_import
 
 from django.contrib.auth.models import AbstractUser
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
