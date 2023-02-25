6880140a2f5c838332e1272743849d16b03e4cfe
bhandariroshan <brishi98@gmail.com>
2023-02-25 13:10:06 -0500

    Update models.py

--- fbstats/users/models.py
+++ fbstats/users/models.py
@@ -4,11 +4,9 @@ from __future__ import unicode_literals, absolute_import
 from django.contrib.auth.models import AbstractUser
 from django.urls import reverse
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
 
-@python_2_unicode_compatible
 class User(AbstractUser):
 
     # First Name and Last Name do not cover name patterns
